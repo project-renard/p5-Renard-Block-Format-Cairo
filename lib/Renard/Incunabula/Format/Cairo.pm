40831189f2a4b2676dc9b888542d2eb3ea18a317
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-01-04 13:17:59 -0600

    Update doc

---
+++ lib/Renard/Incunabula/Format/Cairo.pm
@@ -0,0 +1,11 @@
+package Renard::Incunabula::Format::Cairo;
+# ABSTRACT: A document format for Cairo surfaces
+
+use Moo;
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Format-Cairo/>
